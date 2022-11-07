ec0c020cce6453edc84554596ad59f1648377d7f
vivekkumar854301 <vivekkumar2020@vitbhopal.ac.in>
2022-11-07 16:21:21 +0530

    add

---
+++ login/loging/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+import mysql.connector as sql
+em=''
+pwd=''
+# Create your views here.
+def loginaction(request):
+    global em,pwd
+    if request.method=="POST":
+        m=sql.connect(host='localhost',user='root',passwd="Purnea@123",database='login')
+        cursor=m.cursor()
+        d=request.POST
+        for key,value in d.items():
+            if key=="email":
+                em=value
+            if key=="password":
+                pwd=value
+        
+        c="select * from user where email='{}' and password='{}'".format(em,pwd)
+        cursor.execute(c)
+        t=tuple(cursor.fetchall())
+        if t==():
+            return render(request,'error.html')
+        else:
+            return render(request,"welcome.html")
+        
+    return render(request,'loging_page.html')
